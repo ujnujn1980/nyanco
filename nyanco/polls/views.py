9d1c0770fac8c88cdefb8abcda120ac1d3e6ddc4
ujnujn1980 <ujnujn1980@gmail.com>
2018-03-06 21:41:16 +0900

    add 404 not found

--- nyanco/polls/views.py
+++ nyanco/polls/views.py
@@ -4,30 +4,18 @@ from .models import Question
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    """
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    #return HttpResponse(output)
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-    """
-
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    #return HttpResponse("You're looking at question %s." % question_id)
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
